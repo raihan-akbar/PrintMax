67ee60eaca3ff7091203447f0ba7a29ab0f086ff
Raihan Akbar <79452379+raihan-akbar@users.noreply.github.com>
2025-06-13 07:06:31 +0700

    dark mode on footer

--- application/views/public/parts/footer.php
+++ application/views/public/parts/footer.php
@@ -1,10 +1,10 @@
 <!-- Footer START -->
-<footer class="bg-white rounded-lg shadow">
+<footer class="bg-slate-50 dark:bg-slate-950 rounded-lg shadow">
 	<div class="w-full max-w-screen-xl mx-auto pt-2">
-		<p class="text-center text-neutral-600 text-sm">Ghina Nur Agsya</p>
-		<hr class="my-2 border-neutral-200 sm:mx-auto" />
+		<p class="text-center text-neutral-600 dark:text-neutral-500 text-sm">Ghina Nur Agsya</p>
+		<hr class="my-2 border-neutral-200 dark:border-neutral-900 sm:mx-auto" />
 		<span class="block text-sm text-gray-500 text-center pb-4">
-			<a href="https://foxlabs.id/" class="hover:underline">Fox Labs ID <?= date("Y") ?> &copy; </a>All Rights Reserved.
+			<a href="https://foxlabs.id/" class="hover:underline">PrintMax <?= date("Y") ?> &copy; </a>All Rights Reserved.
 			<p class="text-center text-xs font-light casc" style="font-size: 5px;"><?= $this->session->userdata('agent_token') ?></p>
 		</span>
 	</div>
